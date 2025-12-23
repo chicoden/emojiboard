ce42931fff2e39c2974b3f08ffb97297b477838b
Elijah Bevers <elijah.bevers@oit.edu>
2025-12-23 13:17:17 -0800

    added log message for when connected to database, made small formatting adjustments, finally removed unused code

--- emojiboard.py
+++ emojiboard.py
@@ -42,13 +42,13 @@ class EmojiRegistrationRecord:
         self.emoji_weight = row[4]
 
     def __repr__(self):
-        return f"<EmojiRegistrationRecord"             \
+        return "<EmojiRegistrationRecord"              \
             f" is_default={repr(self.is_default)}"     \
             f" is_animated={repr(self.is_animated)}"   \
             f" emoji_id={repr(self.emoji_id)}"         \
             f" emoji_name={repr(self.emoji_name)}"     \
             f" emoji_weight={repr(self.emoji_weight)}" \
-        f">"
+        ">"
 
 async def post_leaderboard(guild: discord.Guild, tracked_emoji: List[EmojiRegistrationRecord], start_timestamp: datetime.datetime):
     self_member = guild.get_member(client.user.id)
@@ -85,6 +85,7 @@ async def mask_vowels(interaction: discord.Interaction, message: str):
 async def task_post_leaderboards():
     try:
         async with await mysql.connector.aio.connect(**EMO_DB_CONFIG) as db:
+            log.info(f"{EMO_DB_CONFIG["database"]}: connected")
             async with asyncio.TaskGroup() as per_guild:
                 log.info("creating leaderboard tasks")
                 start_timestamp = datetime.datetime.now(datetime.UTC) - datetime.timedelta(hours=24)
@@ -104,8 +105,8 @@ async def task_post_leaderboards():
 
                         if is_tracked:
                             await emoji_cursor.execute('''
-                                SELECT emoji.is_default, emoji.is_animated, emoji.emoji_id, emoji.emoji_name, tracked_emoji.emoji_weight FROM emoji
-                                    INNER JOIN tracked_emoji ON tracked_emoji.emoji_index = emoji.emoji_index
+                                SELECT emoji.is_default, emoji.is_animated, emoji.emoji_id, emoji.emoji_name, tracked_emoji.emoji_weight
+                                    FROM emoji INNER JOIN tracked_emoji ON tracked_emoji.emoji_index = emoji.emoji_index
                                     WHERE tracked_emoji.guild_id = %s;
                             ''', (guild_id,))
                             tracked_emoji = [EmojiRegistrationRecord(row) for row in await emoji_cursor.fetchall()]
@@ -114,36 +115,6 @@ async def task_post_leaderboards():
 
     except mysql.connector.errors.Error as error:
         log.error(f"{EMO_DB_CONFIG["database"]}: connection failed. {error}")
-
-# @tasks.loop(hours=24)
-# async def task_make_leaderboard():
-#     start_timestamp = datetime.datetime.now(datetime.UTC) - datetime.timedelta(hours=24)
-#     for guild in client.guilds:
-#         bot_member = guild.get_member(client.user.id)
-#         emojiboard_channel = discord.utils.get(guild.text_channels, name="emojiboard")
-#         if emojiboard_channel is None:
-#             log.info(f"creating topic channel for guild {guild.name}")
-#             emojiboard_channel = await guild.create_text_channel(name="emojiboard", topic="Dedicated emojiboard channel")
-#         winner = None
-#         winning_score = 0
-#         for channel in guild.text_channels:
-#             channel_permissions = channel.permissions_for(bot_member)
-#             if not channel_permissions.read_message_history:
-#                 continue
-#             async for message in channel.history(after=start_timestamp):
-#                 score = 0
-#                 for reaction in message.reactions:
-#                     emoji = reaction.emoji
-#                     if type(emoji) is discord.Emoji and emoji.name.lower() == "kekw":
-#                         score = max(score, reaction.count)
-#                 if score > 0 and score >= winning_score:
-#                     winner = message
-#                     winning_score = score
-#         if winner is not None:
-#             await emojiboard_channel.send(f"{winner.author.mention} wins with {winning_score} kekw reactions")
-#             await winner.forward(emojiboard_channel)
-#         else:
-#             await emojiboard_channel.send(f"No winners {BotEmoji.KEKKED_SADGE}")
 
 @client.event
 async def on_ready():
