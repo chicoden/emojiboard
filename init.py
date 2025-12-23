941861c3356d47f5fc677b2492da25ef1f216da1
Elijah Bevers <elijah.bevers@oit.edu>
2025-12-22 21:13:33 -0800

    making two connections is failing so try doing it in one

--- init.py
+++ init.py
@@ -5,20 +5,15 @@ from config import *
 
 async def main():
     try:
-        print("initializing...")
         async with await mysql.connector.aio.connect(**EMO_DB_CONFIG) as db:
+            print("initializing...")
             async with await db.cursor() as cursor:
                 #await cursor.execute('''
                 #''')
                 pass
-        print("initialization complete.")
+            print("initialization complete.")
 
-    except mysql.connector.errors.Error as error:
-        print(f"{EMO_DB_CONFIG["database"]}: connection failed. {error}")
-
-    try:
-        print("verifying initialization...")
-        async with await mysql.connector.aio.connect(**EMO_DB_CONFIG) as db:
+            print("verifying initialization...")
             async with await db.cursor() as cursor:
                 await cursor.execute('''
                     SELECT table_name FROM information_schema.tables WHERE table_schema = %s;
